b1a3602cce2d58213fe43e44eb51b5439c68d602
Francisco Antonio <ffrancisco.skate.98@gmail.com>
2023-05-26 10:22:53 +0200

    init project

---
+++ worldCupWinners_Linq/Controllers/WorldCupController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace worldCupWinners_Linq.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WorldCupController : ControllerBase
+{
+    
+}
